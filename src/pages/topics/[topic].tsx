56c84c56b50dc4746939748644216a07f7f204a2
Matthew Quijano <mq022002@gmail.com>
2024-02-04 11:13:05 -0500

    Refactor: Small adjustments to UI and color scheme
    - Display "Incomplete ❌", to contrast a "Complete ✅" on Coursera.
    - Applied to topic title as well.

--- src/pages/topics/[topic].tsx
+++ src/pages/topics/[topic].tsx
@@ -22,21 +22,34 @@ export default function TopicPage() {
   return (
     <BaseLayout>
       <div className="w-7/12 rounded-lg border-2 border-solid p-10 text-center">
-        <h1 className="text-3xl font-bold">{topic.title}</h1>
+        <div className="grid grid-cols-6 items-center">
+          <h1 className="col-span-5 flex justify-start text-3xl font-bold">
+            {topic.title}
+          </h1>
+          <div className="flex justify-end">
+            <span className="decoration-5 rounded-full border border-solid border-black bg-slate-200 p-1.5 text-xs font-bold">
+              Incomplete ❌
+            </span>
+          </div>
+        </div>
         <p className="border-b-4 py-3">{topic.description}</p>
         <div className="flex flex-col text-start">
           {topic.chapters.map((chapter, index) => (
             <Link
               key={index}
               href={`/chapters/${chapter.title}`}
-              className="px-3 pt-3 hover:bg-cyan-200"
+              className="px-3 pt-3 hover:bg-slate-200"
             >
               <div>
-                <div className="grid grid-cols-6">
+                <div className="grid grid-cols-6 items-center">
                   <h3 className="col-span-5 text-xl font-bold">
                     {chapter.title}
                   </h3>
-                  <p> Complete?:❌ </p>
+                  <div className="flex justify-end">
+                    <span className="decoration-5 rounded-full border border-solid border-black bg-slate-200 p-1.5 text-xs font-bold">
+                      Incomplete ❌
+                    </span>
+                  </div>
                 </div>
                 <div></div>
                 <p className="border-b-2 pb-3">{chapter.description}</p>
