6a3cfd803125320e2d88ae244e5b5d8b33bd3dba
Matthew Quijano <mq022002@gmail.com>
2024-02-04 02:06:00 -0500

    Refactor: UI elements to look more similar to Coursera
    - Added a border around topics/chapters, similar to how Coursera has a border around their weekly content.
    - Set a percentage-width to maintain uniformity throughout page changes.
    - Added a hardcoded element that states whether a chapter was completed or not.

--- src/pages/topics/[topic].tsx
+++ src/pages/topics/[topic].tsx
@@ -21,22 +21,29 @@ export default function TopicPage() {
 
   return (
     <BaseLayout>
-      <h1 className="text-3xl font-bold">{topic.title}</h1>
-      <p className="border-b-4 py-3">{topic.description}</p>
-      <h2 className="text-2xl font-bold">Chapters</h2>
-      <div className="flex flex-col text-center">
-        {topic.chapters.map((chapter, index) => (
-          <Link
-            key={index}
-            href={`/chapters/${chapter.title}`}
-            className="p-4 hover:bg-gray-100"
-          >
-            <div>
-              <h3 className="text-xl font-bold">{chapter.title}</h3>
-              <p>{chapter.description}</p>
-            </div>
-          </Link>
-        ))}
+      <div className="w-7/12 rounded-lg border-2 border-solid p-10 text-center">
+        <h1 className="text-3xl font-bold">{topic.title}</h1>
+        <p className="border-b-4 py-3">{topic.description}</p>
+        <div className="flex flex-col text-start">
+          {topic.chapters.map((chapter, index) => (
+            <Link
+              key={index}
+              href={`/chapters/${chapter.title}`}
+              className="px-3 pt-3 hover:bg-cyan-200"
+            >
+              <div>
+                <div className="grid grid-cols-6">
+                  <h3 className="col-span-5 text-xl font-bold">
+                    {chapter.title}
+                  </h3>
+                  <p> Complete?:❌ </p>
+                </div>
+                <div></div>
+                <p className="border-b-2 pb-3">{chapter.description}</p>
+              </div>
+            </Link>
+          ))}
+        </div>
       </div>
     </BaseLayout>
   );
