d02ff654f49a485406bf3cde903f6cac165167d4
Matthew Quijano <mq022002@gmail.com>
2024-02-09 07:04:10 -0500

    Add: Firebase configuration file
    Co-authored-by: Andrew <andrew76280@gmail.com>

---
+++ src/pages/firebase/config.ts
@@ -0,0 +1,19 @@
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+// STORE THE VALUES FOR THESE IN .env.local. Ngl, it doesn't really matter for Firebase though, but still.
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+};
+
+// This checks if there are any apps already initialized. If not, it inisializes the app. If so, it gets the app.
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+
+const auth = getAuth(app);
+
+export { app, auth };
