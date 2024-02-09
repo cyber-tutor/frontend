6b08027ec06c153a70338e645f865363b1dc0440
Matthew Quijano <mq022002@gmail.com>
2024-02-09 07:08:35 -0500

    Add: SignUpForm component and sign-up.tsx to display it
    Co-authored-by: Andrew <andrew76280@gmail.com>

---
+++ src/components/SignUpForm.tsx
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { auth } from "../pages/firebase/config";
+import { useRouter } from "next/router";
+
+const SignUpForm = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [createUserWithEmailAndPassword] =
+    useCreateUserWithEmailAndPassword(auth);
+  const router = useRouter();
+
+  const handleSignUp = async (event: React.FormEvent) => {
+    // This prevents the default behavior of a form submission is to reload the page. We don't want that because of async behavior.
+    event.preventDefault();
+    // This is the try-catch block that will handle the actual sign-up process. If it succeeds, they will be redirected to the sign-in page. If it fails, well, nothing really at the moment on the front end, YET. It logs the error to the console though.
+    try {
+      const res = await createUserWithEmailAndPassword(email, password);
+      setEmail("");
+      setPassword("");
+      router.push("/users/sign-in");
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSignUp} className="space-y-6">
+      <div>
+        <label htmlFor="email" className="text-start">
+          Email
+        </label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+          className="flex w-full justify-center rounded p-1"
+        />
+      </div>
+      <div>
+        <label htmlFor="password" className="text-start">
+          Password
+        </label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+          className="flex w-full justify-center rounded p-1"
+        />
+      </div>
+      <button
+        type="submit"
+        className="flex w-full justify-center rounded bg-blue-500 py-1 text-white"
+      >
+        Sign Up
+      </button>
+    </form>
+  );
+};
+
+export default SignUpForm;
