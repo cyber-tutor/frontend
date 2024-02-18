69f2bf1c33fffab7f9f550f6e0700c51ba81764d
Matthew Quijano <mq022002@gmail.com>
2024-02-18 02:51:28 -0500

    Add: getTopics function to fetch from Realtime Database
    - When using Firebase Cloud Functions, it operates with admin perms, bypassing the rules in our database.
    - You need to deploy the function, and for that to happen, we needed to put the project on the Blaze plan.
    - After that, it generates a link. KEEP THE LINK SAFE. Anyone with the link can easily view any data related to what we were fetching. I put in a .env.local.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,3 +1,5 @@
+// MQ: Any commented code and documentation was here to begin with. I'll remove later once I understand what needs to be here and what doesn't after setting up Firebase Cloud Functions.
+
 /**
  * Import function triggers from their respective submodules:
  *
@@ -7,8 +9,10 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+// import {onRequest} from "firebase-functions/v2/https";
+// import * as logger from "firebase-functions/logger";
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
@@ -17,3 +21,29 @@ import * as logger from "firebase-functions/logger";
 //   logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+admin.initializeApp();
+
+// Self-explanatory. This function fetches the topics from the Realtime Database.
+// Why this, instead of setting rules to have read = true? Because we don't want those annoying reminder emails, and more importantly, we don't want to expose our database to the public. Now we can have strict rules and still fetch the data.
+export const getTopics = functions.https.onRequest(
+  async (request, response) => {
+    // This is a CORS header that allows the function to be called from any domain.
+    response.set("Access-Control-Allow-Origin", "*");
+    // This is a CORS header that allows the function to be called with the GET method.
+    response.set("Access-Control-Allow-Methods", "GET");
+
+    try {
+      // This fetches the topics from the Realtime Database.
+      const topicsSnapshot = await admin
+        .database()
+        .ref("/topics")
+        .once("value");
+      const topics = topicsSnapshot.val();
+      response.json(topics);
+    } catch (error) {
+      console.error("uh oh, fetch failed 🦧:", error);
+      response.status(500).send(error);
+    }
+  },
+);
