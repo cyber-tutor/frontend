f089b74140fafb8e1e3f009e4e227dbe31309df3
colmak <69098858+colmak@users.noreply.github.com>
2024-02-27 21:13:48 -0500

    Add admin page with base layout

---
+++ src/pages/admin/index.tsx
@@ -0,0 +1,19 @@
+// index.tsx
+import Head from "next/head";
+import { BaseLayout } from "../layouts/baseLayout";
+
+export default function Home() {
+
+  return (
+    <> 
+      <Head>
+        <title>Cyber Tutor</title>
+        <meta name="description" content="Cyber Tutor" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <BaseLayout>
+        <p>Select a topic from the menu.</p>
+      </BaseLayout>
+    </>
+  );
+}
