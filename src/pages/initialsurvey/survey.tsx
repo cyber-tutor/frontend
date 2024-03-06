e76190a2abb2506242f44a0da7c450726fca4af6
Andrew <andrew76280@gmail.com>
2024-03-06 15:41:20 -0500

    Pushes user's initial survey results to firebase

--- src/pages/initialsurvey/survey.tsx
+++ src/pages/initialsurvey/survey.tsx
@@ -5,6 +5,7 @@ import React, { useEffect, useState } from 'react';
 import * as Survey from 'survey-react';
 import 'survey-react/survey.css';
 import queryUserDocument from '../firebase/firebase_functions';
+import { set } from 'firebase/database';
 
 Survey.StylesManager.applyTheme("default");
 
@@ -38,10 +39,12 @@ export default function SurveyComponent(): JSX.Element {
   const survey = new Survey.Model(surveyJson);
   const router = useRouter();
   const [isComplete, setIsComplete] = useState(false);
+  const [result, setResult] = useState<any>(null);
 
   const onComplete = (result: any) => {
-    console.log(result.data);
     setIsComplete(true);
+    setResult(JSON.stringify(result.data));
+    console.log(JSON.stringify(result.data)); 
   };
 
   useEffect(() => {
@@ -55,23 +58,20 @@ export default function SurveyComponent(): JSX.Element {
         queryUserDocument(userId).then((docSnapshot) => {
           if (docSnapshot) {
 
-            updateDoc(docSnapshot.ref, { initialSurveyComplete: true })
+            updateDoc(docSnapshot.ref, { initialSurveyResults: result, initialSurveyComplete: true })
               .then(() => {
                 console.log('Document successfully updated');
 
-
-
                 if(docSnapshot.data().group === "experimental"){
                   router.push('/initialsurvey/generating');
-                }
-                else{
+                } else{
                   router.push('/');
                 }
               })
               .catch((error) => {
                 console.error('Error updating document: ', error);
               });
-          } else {
+          }else {
             console.log('No document found or error occurred');
           }
         });
