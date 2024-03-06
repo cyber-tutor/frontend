c6e0a9be7c5a9918e4de9a051c96d09aad53c140
Andrew <andrew76280@gmail.com>
2024-03-05 21:32:11 -0500

    Added base survey js questions to be changed

--- src/pages/initialsurvey/survey.tsx
+++ src/pages/initialsurvey/survey.tsx
@@ -1,12 +1,58 @@
 import React from 'react';
+import * as Survey from 'survey-react';
+import 'survey-react/survey.css';
 
-export default function Survey () {
+Survey.StylesManager.applyTheme("default");
 
-
-    return (
-        <div>
-            Survey js survey goes here
-        </div>
-    );
+const surveyJson = {
+  pages: [
+    {
+      name: "page1",
+      elements: [
+        {
+          type: "text",
+          name: "question1",
+          title: "What is your name?"
+        }
+      ]
+    },
+    {
+      name: "page2",
+      elements: [
+        {
+          type: "radiogroup",
+          name: "question2",
+          title: "What is your favorite programming language?",
+          choices: ["JavaScript", "Python", "TypeScript", "Java", "C#"]
+        }
+      ]
+    },
+    {
+      name: "page3",
+      elements: [
+        {
+          type: "comment",
+          name: "question3",
+          title: "What do you like most about the selected programming language?"
+        }
+      ]
+    }
+  ]
 };
 
+export default function SurveyComponent(): JSX.Element {
+  const survey = new Survey.Model(surveyJson);
+
+  const onComplete = (result: any) => {
+    console.log(result.data);
+  };
+
+  return (
+    <div>
+      <Survey.Survey
+        model={survey}
+        onComplete={onComplete}
+      />
+    </div>
+  );
+}
