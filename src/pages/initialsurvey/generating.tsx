2e9a516ae448f1fe7022d25cc567cafb12c7a60c
Andrew <andrew76280@gmail.com>
2024-03-06 10:15:41 -0500

    After initial survey is complete and the user is in the experimental group, route them to this page to generate ai content for them

---
+++ src/pages/initialsurvey/generating.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+interface Props {
+
+}
+
+const MyComponent: React.FC<Props> = ({}) => {
+
+
+    return (
+        <div>
+            Ai is generating your content... Loading...
+        </div>
+    );
+};
+
+export default MyComponent;
