04a475eb793dcbf537fe86ee9f41be458880ac70
colmak <69098858+colmak@users.noreply.github.com>
2024-03-10 14:38:21 -0400

    Refactor UI layout and add NextUIProvider
    Co-authored-by: Andrew Krasuski <andrew5701@users.noreply.github.com>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,12 @@
 import { type AppType } from "next/app";
+import {NextUIProvider} from "@nextui-org/react";
 
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return <NextUIProvider><Component {...pageProps} /></NextUIProvider>;
 };
 
 export default api.withTRPC(MyApp);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,14 +2,13 @@ import Head from "next/head";
 import { BaseLayout } from "./layouts/baseLayout";
 import { auth } from "./firebase/config";
 import { useAuthState } from "react-firebase-hooks/auth";
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import dynamic from "next/dynamic";
 import queryUserDocument from "./firebase/firebase_functions";
 import { DocumentData } from "firebase/firestore";
 import { useRouter } from "next/router";
 
-
-const ReactPlayer = dynamic(() => import('react-player'), { ssr: false });
+const ReactPlayer = dynamic(() => import("react-player"), { ssr: false });
 
 export default function Home() {
   const [user, loading, error] = useAuthState(auth);
@@ -19,7 +18,6 @@ export default function Home() {
   const uid = user ? user.uid : null;
 
   console.log("User Id:", uid);
-
 
   // If user is logged in, query and retreive the reference to their document in the users collection in firestore
   if (uid) {
@@ -32,7 +30,7 @@ export default function Home() {
 
   // Check if user completed initial survey, if not then redirect to initial survey
   if (userDocument && !userDocument.data().initialSurveyComplete) {
-    router.push('/initialsurvey/begin');
+    router.push("/initialsurvey/begin");
   }
 
   return (
@@ -42,26 +40,51 @@ export default function Home() {
         <meta name="description" content="Cyber Tutor" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <BaseLayout>
-        {user ? (
+
+      {user ? (
+        <BaseLayout showSidebar={true}>
           <p>Select a topic from the menu.</p>
-        ) : (
-          <div className="min-h-screen bg-white text-gray-900 flex flex-col justify-center items-center">
-            {typeof window !== 'undefined' && (
-              <ReactPlayer 
-                url="https://www.youtube.com/watch?v=8BoovULyJeg&list=PLVEnBuMmQvXukhIgRrTIwOxWBoLwYYL0A" 
-                playing={true} 
-                controls={true} 
-                className="max-w-full rounded-lg shadow-xl"
-              />
-            )}
-            <p className="mt-8 text-center text-xl md:text-2xl font-semibold">
-              Welcome to CyberTutor! Dive into the world of cyber security and fortify your digital life today. 
-              Explore our curated video series to become a savvy internet user and protect yourself against online threats.
-            </p>
-          </div>
-        )}
-      </BaseLayout>
+        </BaseLayout>
+      ) : (
+        <BaseLayout showSidebar={false}>
+  <div className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-r from-cyan-500 to-blue-500 text-white">
+    <div className="w-full max-w-4xl px-4 py-8 bg-white bg-opacity-90 rounded-xl shadow-2xl">
+      {/* Responsive container */}
+      {typeof window !== "undefined" && (
+        <ReactPlayer
+          url="https://www.youtube.com/watch?v=8BoovULyJeg&list=PLVEnBuMmQvXukhIgRrTIwOxWBoLwYYL0A"
+          playing={false}
+          controls={true}
+          width="100%"
+          height="100%"
+          className="aspect-video rounded-lg shadow-xl"
+        />
+      )}
+    </div>
+    <p className="mt-8 text-center text-xl font-semibold md:text-2xl lg:text-3xl">
+      Welcome to CyberTutor! Dive into the world of cyber security and
+      fortify your digital life today. Explore our curated video series
+      to become a savvy internet user and protect yourself against
+      online threats.
+    </p>
+    <div className="mt-8 flex space-x-4">
+      <button
+        className="rounded-lg bg-blue-700 px-6 py-3 font-bold text-white transition duration-150 ease-in-out hover:bg-blue-800"
+        onClick={() => router.push("/users/sign-in")}
+      >
+        Register
+      </button>
+      <button
+        className="rounded-lg bg-green-700 px-6 py-3 font-bold text-white transition duration-150 ease-in-out hover:bg-green-800"
+        onClick={() => router.push("/users/sign-up")}
+      >
+        Login
+      </button>
+    </div>
+  </div>
+</BaseLayout>
+
+      )}
     </>
   );
 }
