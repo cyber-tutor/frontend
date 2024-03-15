e1d14d4d5bd2259c48c54562391336e665d58e70
Matthew Quijano <mq022002@gmail.com>
2024-03-15 18:58:39 -0400

    refactor: Finalized users collection document generation
    - Progress and proficiency now accurately mapped to topics and chapters.
    - isSuperuser field added for planned access role differentiation.
    - Learned how to utilize Firestore batch to ensure consistency in our database. If any part of the batch fails, the entire batch fails.
    
    Co-authored-by: Andrew Krasuski <andrew5701@users.noreply.github.com>

--- src/pages/firebase/firebase_functions.ts
+++ src/pages/firebase/firebase_functions.ts
@@ -1,16 +1,29 @@
-import { Firestore, doc, addDoc, collection, query, where, getDocs, DocumentData, QueryDocumentSnapshot, updateDoc, getDoc } from 'firebase/firestore';
-import { db } from './config'; 
+import {
+  Firestore,
+  doc,
+  addDoc,
+  collection,
+  query,
+  where,
+  getDocs,
+  DocumentData,
+  QueryDocumentSnapshot,
+  updateDoc,
+  getDoc,
+  writeBatch,
+  orderBy,
+} from "firebase/firestore";
+import { db } from "./config";
 import { User } from "firebase/auth";
-import { Console } from 'console';
+import { Console } from "console";
 
+export default async function queryUserDocument(
+  userIdString: string,
+): Promise<DocumentData | null> {
+  const usersCollectionRef = collection(db, "users");
 
-export default async function queryUserDocument(userIdString: string): Promise<DocumentData | null> {
-  const usersCollectionRef = collection(db, 'users');
-
-
-  // Hardcoded user ID for testing 
-  const q = query(usersCollectionRef, where('userId', '==', userIdString)); 
-
+  // Hardcoded user ID for testing
+  const q = query(usersCollectionRef, where("userId", "==", userIdString));
 
   try {
     const querySnapshot = await getDocs(q);
@@ -19,15 +32,13 @@ export default async function queryUserDocument(userIdString: string): Promise<D
     //   return null;
     // }
 
-
     const firstDoc = querySnapshot.docs[0];
     if (firstDoc) {
-      console.log('Success.');
-    //   return firstDoc.data();
-    return firstDoc;
-    
+      console.log("Success.");
+      //   return firstDoc.data();
+      return firstDoc;
     } else {
-      console.log('No matching documents.');
+      console.log("No matching documents.");
       return null;
     }
   } catch (error) {
@@ -36,72 +47,116 @@ export default async function queryUserDocument(userIdString: string): Promise<D
   }
 }
 
-
-export async function handleVideoEnd(played: number, userDocumentId: string): Promise<void> {
+export async function handleVideoEnd(
+  played: number,
+  userDocumentId: string,
+): Promise<void> {
   try {
-
     if (!userDocumentId) {
       console.error("User document ID is undefined.");
       return;
     }
 
     // Set videoWatched to true in Firestore
-    const videoDocRef = doc(db, 'users', userDocumentId);
+    const videoDocRef = doc(db, "users", userDocumentId);
     await updateDoc(videoDocRef, {
-      videoCompleted: true
+      videoCompleted: true,
     });
-
-
   } catch (error) {
     console.error("Error updating video progress:", error);
   }
 }
 
 export async function isWatched(userDocumentId: string): Promise<boolean> {
-    try {
-      if (!userDocumentId) {
-        console.error("User document ID is undefined.");
-        return false; 
-      }
-  
-      const videoDocRef = doc(db, 'users', userDocumentId);
-      const docSnap = await getDoc(videoDocRef);
-  
-      if (docSnap.exists()) {
-    
-        const videoCompleted = docSnap.data().videoCompleted || false;
-        return videoCompleted; 
-      } else {
-        console.log("No such document!");
-        return false; 
-      }
-    } catch (error) {
-  
-      return false; 
+  try {
+    if (!userDocumentId) {
+      console.error("User document ID is undefined.");
+      return false;
     }
-  }
 
-  export async function createUserDocument(user: User, userName: string): Promise<void> {
-    const isExperimental = Math.random() < 0.5;
-    const group = isExperimental ? "experimental" : "control";
-  
-    const docRef = await addDoc(collection(db, "users"), {
-      userId: user.uid,
-      group: group,
-      name: userName|| "",
-      initialSurveyComplete: false,
-      progress: {
-        0: { 0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0 },
-        1: { 0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0 },
-        2: { 0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0 },
-        3: { 0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0 },
-        4: { 0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0 },
-        5: { 0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0 }
-      },
-      proficiency: { 0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0 },
-      scoresQuiz: { 0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0 },
-      scoresTest: { 0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0 }
+    const videoDocRef = doc(db, "users", userDocumentId);
+    const docSnap = await getDoc(videoDocRef);
+
+    if (docSnap.exists()) {
+      const videoCompleted = docSnap.data().videoCompleted || false;
+      return videoCompleted;
+    } else {
+      console.log("No such document!");
+      return false;
+    }
+  } catch (error) {
+    return false;
+  }
+}
+
+export async function createUserDocument(
+  user: User,
+  userName: string,
+): Promise<void> {
+  const isExperimental = Math.random() < 0.5;
+  const group = isExperimental ? "experimental" : "control";
+
+  // Start a batch operation. This makes it so that if any part of the batch fails, the entire batch fails.
+  const batch = writeBatch(db);
+
+  const userRef = doc(collection(db, "users"));
+  batch.set(userRef, {
+    userId: user.uid,
+    group: group,
+    name: userName || "",
+    initialSurveyComplete: false,
+    isSuperuser: false,
+  });
+
+  const topicsCollectionRef = collection(db, "topics");
+  try {
+    const topicsSnapshot = await getDocs(
+      query(topicsCollectionRef, orderBy("order")),
+    );
+    const chapterPromises = topicsSnapshot.docs.map(async (topicDoc) => {
+      const topicId = topicDoc.id;
+
+      const proficiencyRef = doc(
+        collection(db, "users", userRef.id, "proficiency"),
+        topicId,
+      );
+      batch.set(proficiencyRef, {
+        proficiency: 0,
+      });
+
+      const chaptersCollectionRef = collection(
+        db,
+        "topics",
+        topicId,
+        "chapters",
+      );
+      const chaptersSnapshot = await getDocs(chaptersCollectionRef);
+      chaptersSnapshot.forEach((chapterDoc) => {
+        const chapterId = chapterDoc.id;
+        const progressRef = doc(
+          collection(db, "users", userRef.id, "progress"),
+          chapterId,
+        );
+        batch.set(progressRef, {
+          complete: false,
+          quizScores: [],
+        });
+      });
     });
-    console.log("Document written with user ID: ", user.uid);
-    
-  };
+
+    // Wait for all chapter fetching and adding to batch to complete
+    await Promise.all(chapterPromises);
+
+    // Commit the batch
+    await batch.commit();
+    console.log(
+      "User document and subcollections created with ID: ",
+      userRef.id,
+    );
+  } catch (error) {
+    console.error(
+      "Error fetching topics or chapters, or error in batch write:",
+      error,
+    );
+  }
+}
