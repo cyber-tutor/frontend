52ea9f7712b2ab81a0981127d90631246df32ccc
Andrew <andrew76280@gmail.com>
2024-03-16 15:22:03 -0400

    After a certain amount of time, for now and for testing 3 seconds, the next button appears. When it is clicked complete is set to true, and the users attempt is saved

--- src/pages/topics/[topic]/[chapters]/[chapter].tsx
+++ src/pages/topics/[topic]/[chapters]/[chapter].tsx
@@ -1,19 +1,17 @@
 import { BaseLayout } from "../../../layouts/baseLayout";
 import { useRouter } from "next/router";
 import { useState, useEffect } from "react";
-import { getDatabase, ref, get, set } from "firebase/database";
 import { Survey } from "survey-react-ui";
 import { Model } from "survey-core";
 import "survey-core/defaultV2.min.css";
 import ReactPlayer from "react-player";
 import getVideoDuration from "~/components/youtube_data";
 import { db, auth } from "~/pages/firebase/config";
-import firebase from "firebase/app";
-import queryUserDocument from "~/pages/firebase/firebase_functions";
-import { DocumentData, doc, getDoc, updateDoc } from "firebase/firestore";
-import { handleVideoEnd, isWatched } from "~/pages/firebase/firebase_functions";
+import { DocumentData, doc, getDoc, updateDoc, arrayUnion } from "firebase/firestore";
+import { findUserDocId, handleVideoEnd, isWatched } from "~/pages/firebase/firebase_functions";
 import TimerComponent from "~/components/Timer";
 import DynamicSurvey from "../../../../components/DynamicSurvey";
+
 
 type Chapter = {
   chapterId: string;
@@ -25,17 +23,6 @@ type Chapter = {
   controlGroupImageURLs: string[];
   experimentalGroupImageURLs: string[];
 };
-
-// Old Question
-
-// type Question = {
-//   questionId: string;
-//   questionTitle: string;
-//   questionDifficulty: string;
-//   options: Option[];
-// };
-
-// New Question
 
 type Question = {
   questionId: string;
@@ -55,15 +42,11 @@ export default function ChapterPage() {
   const [chapter, setChapter] = useState<Chapter | null>(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
-
-  // Youtube states
   const [played, setPlayed] = useState(0);
-  const [duration, setDuration] = useState("");
   const [userDocument, setUserDocument] = useState<DocumentData | null>(null);
   const [isVideoWatched, setIsVideoWatched] = useState(false);
-
-  // For tracking user time spent on current page
   const [secondsElapsed, setSecondsElapsed] = useState(0);
+  const [progressComplete, setProgressComplete] = useState(false);
 
   const router = useRouter();
   const { topic: topicId, chapter: chapterId } = router.query;
@@ -109,6 +92,13 @@ export default function ChapterPage() {
     fetchChapter();
   }, [topicId, chapterId]);
 
+  useEffect(() => {
+    // Check if 1 minute has passed
+    if (secondsElapsed >= 3) {
+      setProgressComplete(true);
+    }
+  }, [secondsElapsed]);
+
   if (loading)
     return (
       <BaseLayout>
@@ -128,33 +118,19 @@ export default function ChapterPage() {
       </BaseLayout>
     );
 
-  // Retreive the id of the user logged in and print it
   const user = auth.currentUser;
   const uid = user ? user.uid : null;
   console.log("User:", uid);
-
-  // If user is logged in, query and retreive the reference to their document in the users collection in firestore
-  if (uid) {
-    queryUserDocument(uid).then((userDocument) => {
-      setUserDocument(userDocument);
-    });
-  }
-
-  // Check if the isWatched key value pair is set to true in the user's document in firestore to decide if the next button should be visible
-  (async () => {
-    if (await isWatched(userDocument?.id)) {
-      setIsVideoWatched(true);
-    }
-  })();
 
   return (
     <BaseLayout>
       <h1 className="text-3xl font-bold">{chapter.chapterTitle}</h1>
       <p className="border-b-4 py-3">{chapter.chapterDescription}</p>
       <div className="mx-auto w-full overflow-y-auto">
-        {/* Later on, AFTER we set up user profiles, we want to implement conditional logic to determine what content we want to display based on what group they are assigned after either signing up/completing initial assessment */}
         {chapter.chapterType === "text" && (
-          <div className="m-4 rounded border p-4 shadow">
+          <div className="m-4 rounded border p-4 shadow"> 
+            {userDocument?.data().id}
+
             {chapter.controlGroupContent}
             {chapter.controlGroupImageURLs[0] && (
               <img
@@ -176,13 +152,6 @@ export default function ChapterPage() {
         )}
         {chapter.chapterType === "video" && (
           <div className="flex aspect-[16/9] flex-grow">
-            {/* <iframe
-              title="YouTube video player"
-              className="h-full w-full"
-              allowFullScreen
-              src={chapter.controlGroupContent}
-            ></iframe> */}
-
             <ReactPlayer
               url={chapter.controlGroupContent}
               onProgress={(progress) => {
@@ -193,44 +162,76 @@ export default function ChapterPage() {
               controls={false}
               onEnded={() => {
                 const playedMinutes = Math.floor(played / 60);
-
                 console.log("video ended");
-
-                // console.log('User Document ID:' , userDocument?.id);
                 handleVideoEnd(playedMinutes, userDocument?.id);
                 setIsVideoWatched(true);
               }}
-              // playing
-              // progressInterval={1000}
               seekTo={20}
             />
           </div>
+          
         )}
-        {isVideoWatched && (
-          <button
-            className="rounded bg-blue-500 px-4 py-2 font-bold text-white transition duration-150 ease-in-out hover:bg-blue-700"
-            onClick={async () => {
-              // Pushes time user was on the page for to firestore
-              const userDocRef = doc(db, "users", userDocument?.id);
-              const minutes = Math.floor(secondsElapsed / 60);
-              const seconds = secondsElapsed % 60;
-              await updateDoc(userDocRef, {
-                timeOnPage: `${minutes}:${String(seconds).padStart(2, "0")}`,
-              });
-              console.log(
-                "User time watched pushed successfully to firestore:",
-                secondsElapsed,
-              );
-            }}
-          >
-            Next
-          </button>
-        )}
+
+{progressComplete && (
+  <button
+    className="rounded bg-blue-500 px-4 py-2 font-bold text-white transition duration-150 ease-in-out hover:bg-blue-700"
+    onClick={async () => {
+      const userDocId = await findUserDocId(uid ?? "");
+      if (typeof chapterId === 'string' && userDocId) {
+        const userDocRef = doc(db, "users", userDocId);
+        const minutes = Math.floor(secondsElapsed / 60);
+        const seconds = secondsElapsed % 60;
+        const timeElapsed = `${minutes}:${String(seconds).padStart(2, "0")}`;
+        await updateDoc(userDocRef, {
+          timeOnPage: timeElapsed,
+        });
+        console.log(
+          "User time watched pushed successfully to firestore:",
+          timeElapsed,
+        );
+
+        const progressRef = doc(db, "users", userDocId, "progress", chapterId);
+        const progressSnapshot = await getDoc(progressRef);
+
+        if (progressSnapshot.exists()) {
+          const progressData = progressSnapshot.data();
+          const attempts = progressData.attempts ?? {};
+          const nextAttemptNumber = Object.keys(attempts).length + 1;
+          const updatedAttempts = {
+            ...attempts,
+            [nextAttemptNumber]: {
+              timeElapsed,
+            },
+          };
+
+          await updateDoc(progressRef, {
+            complete: true,
+            attempts: updatedAttempts,
+          });
+        } else {
+          // If no progress document exists, create the first attempt
+          await updateDoc(progressRef, {
+            complete: true,
+            attempts: {
+              1: {
+                timeElapsed,
+              },
+            },
+          });
+        }
+      }
+    }}
+  >
+    Next
+  </button>
+)}
+
+
+
+
         <br />
-        {/* Converts seconds to minutes and removes the decimal point */}
         video progress: {Math.floor(played / 60)}:
         {String(Math.floor(played % 60)).padStart(2, "0")}
-        {/* Record how long a user spends on the chapter currently open */}
         <br />
         <p>User's time spent on this chapter:</p>
         <TimerComponent
@@ -239,7 +240,6 @@ export default function ChapterPage() {
         />
         {chapter && chapter.chapterType === "assessment" && (
           <div className="w-full px-4 md:px-0">
-            {" "}
             <DynamicSurvey chapterId={chapter.chapterId} userId={userDocument?.data().userId} />
           </div>
         )}
