9026c009ed456cfcf5e750e11b7332bffa19c061
Andrew <andrew76280@gmail.com>
2024-03-21 13:35:13 -0400

    Added a survey.tsx file for demographic survey

---
+++ src/pages/demographicsurvey/survey.tsx
@@ -0,0 +1,87 @@
+import { getAuth } from 'firebase/auth';
+import { doc, getDocs, collection, getFirestore, writeBatch } from 'firebase/firestore';
+import { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react';
+import * as Survey from 'survey-react';
+import 'survey-react/survey.css';
+import { db } from '../firebase/config';
+
+Survey.StylesManager.applyTheme("default");
+
+export default function SurveyComponent(): JSX.Element {
+  const router = useRouter();
+  const [isComplete, setIsComplete] = useState<boolean>(false);
+  const [result, setResult] = useState<any>(null);
+  const [surveyJson, setSurveyJson] = useState<Survey.Model | null>(null);
+
+  useEffect(() => {
+    const fetchSurveyJson = async () => {
+      const surveyCollectionRef = collection(db, "demographicSurveyQuestions");
+      const surveyDocsSnapshot = await getDocs(surveyCollectionRef);
+
+      const surveyQuestions = surveyDocsSnapshot.docs.map((docSnapshot) => {
+        const data = docSnapshot.data();
+        const { question, choices } = data;
+        return {
+          type: "radiogroup",
+          name: docSnapshot.id,
+          title: question,
+          isRequired: true,
+          choices: Object.keys(choices).map((key) => ({
+            value: key,
+            text: choices[key]
+          }))
+        };
+      });
+
+      const surveyJson = {
+        title: "Demographic Survey",
+        showProgressBar: "bottom",
+        pages: [{ elements: surveyQuestions }]
+      };
+
+      setSurveyJson(new Survey.Model(surveyJson));
+    };
+
+    fetchSurveyJson();
+  }, []);
+
+  const onComplete = async (surveyResult: Survey.Model) => {
+    setIsComplete(true);
+    const surveyData = surveyResult.data;
+    setResult(surveyData);
+
+
+    const auth = getAuth();
+    const user = auth.currentUser;
+
+    if (user) {
+      const batch = writeBatch(db);
+
+      Object.entries(surveyData).forEach(([questionId, answer]) => {
+        const userResponseDocRef = doc(db, "demographicSurveyQuestions", questionId, "users", user.uid);
+        batch.set(userResponseDocRef, { answer });
+      });
+
+      try {
+        await batch.commit();
+        console.log("Survey responses successfully written to Firestore.");
+        router.push('/thank-you'); 
+      } catch (error) {
+        console.error("Error writing survey responses: ", error);
+      }
+    } else {
+      console.error("User is not authenticated.");
+    }
+  };
+
+  return (
+    <div>
+      {surveyJson ? (
+        <Survey.Survey model={surveyJson} onComplete={onComplete} />
+      ) : (
+        <div>Loading survey...</div>
+      )}
+    </div>
+  );
+}
