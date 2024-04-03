f06c670dac14c5b745b3c616d40635cf46e1b899
colmak <69098858+colmak@users.noreply.github.com>
2024-04-02 23:44:12 -0400

    Added more visual interest onto the home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,8 +23,6 @@ export default function Home() {
   // If user is logged in, query and retreive the reference to their document in the users collection in firestore
   if (uid) {
     queryUserDocument(uid).then((userDocument) => {
-
-
       setUserDocument(userDocument);
     });
   }
@@ -46,30 +44,30 @@ export default function Home() {
           <p>Select a topic from the menu.</p>
         </BaseLayout>
       ) : (
-        <div className="flex min-h-screen flex-col items-center justify-center bg-white text-gray-900">
+        <div className="flex min-h-screen flex-col items-center justify-center bg-gray-200 p-4 text-gray-900">
           {typeof window !== "undefined" && (
             <ReactPlayer
               url="https://www.youtube.com/watch?v=8BoovULyJeg&list=PLVEnBuMmQvXukhIgRrTIwOxWBoLwYYL0A"
               playing={false}
               controls={true}
-              className="max-w-full rounded-lg shadow-xl"
+              className="mb-8 max-w-full rounded-lg shadow-2xl"
             />
           )}
-          <p className="mx-auto mt-8 text-center text-xl font-semibold md:w-1/2 md:text-2xl lg:w-1/2">
+          <p className="mx-auto mt-8 rounded-lg bg-white p-4 text-center text-xl font-semibold shadow-lg  md:text-2xl lg:w-1/2">
             Welcome to CyberTutor! Dive into the world of cyber security and
             fortify your digital life today. Explore our curated video series to
             become a savvy internet user and protect yourself against online
             threats.
           </p>
-          <div className="mt-8 flex space-x-4">
+          <div className="mt-10 flex space-x-6">
             <button
-              className="rounded-lg bg-blue-700 px-6 py-3 font-bold text-white transition duration-150 ease-in-out hover:bg-blue-800"
+              className="rounded-lg bg-blue-700 px-6 py-3 font-bold text-white shadow-lg transition duration-150 ease-in-out hover:bg-blue-800"
               onClick={() => router.push("/users/sign-up")}
             >
               Register
             </button>
             <button
-              className="rounded-lg bg-green-700 px-6 py-3 font-bold text-white transition duration-150 ease-in-out hover:bg-green-800"
+              className="rounded-lg bg-green-700 px-6 py-3 font-bold text-white shadow-lg transition duration-150 ease-in-out hover:bg-green-800"
               onClick={() => router.push("/users/sign-in")}
             >
               Login
