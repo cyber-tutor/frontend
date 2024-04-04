2e611e21c2c2c1aeac6626d1c4aaed5d4c76a4bc
Andrew <andrew76280@gmail.com>
2024-04-04 13:48:23 -0400

    Fixed footer appearance

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -39,53 +39,49 @@ export default function Home() {
         <meta name="description" content="Cyber Tutor" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      {user ? (
-        <BaseLayout>
-          <p>Select a topic from the menu.</p>
-        </BaseLayout>
-      ) : (
-        <div className="flex min-h-screen flex-col items-center justify-center bg-gray-200 p-4 text-gray-900">
-          {typeof window !== "undefined" && (
-            <ReactPlayer
-              url="https://www.youtube.com/watch?v=8BoovULyJeg&list=PLVEnBuMmQvXukhIgRrTIwOxWBoLwYYL0A"
-              playing={false}
-              controls={true}
-              className="mb-8 max-w-full rounded-lg shadow-2xl"
-            />
-          )}
-          <p className="mx-auto mt-8 rounded-lg bg-white p-4 text-center text-xl font-semibold shadow-lg  md:text-2xl lg:w-1/2">
-            Welcome to CyberTutor! Dive into the world of cyber security and
-            fortify your digital life today. Explore our curated video series to
-            become a savvy internet user and protect yourself against online
-            threats.
-          </p>
-          <div className="mt-10 flex space-x-6">
-            <button
-              className="rounded-lg bg-blue-700 px-6 py-3 font-bold text-white shadow-lg transition duration-150 ease-in-out hover:bg-blue-800"
-              onClick={() => router.push("/users/sign-up")}
-            >
-              Register
-            </button>
-            <button
-              className="rounded-lg bg-green-700 px-6 py-3 font-bold text-white shadow-lg transition duration-150 ease-in-out hover:bg-green-800"
-              onClick={() => router.push("/users/sign-in")}
-            >
-              Login
-            </button>
-          </div>
-
-          <footer className="mt-auto w-full border-t border-gray-300 bg-white py-4 text-center text-gray-900">
-            <div className="container mx-auto flex flex-col items-center justify-between space-y-4 px-4 md:flex-row md:space-y-0">
-              <p>&copy; 2024 Cyber Tutor. All rights reserved.</p>
-              <nav>
-                <ul className="flex space-x-4">
-                 
-                </ul>
-              </nav>
+      <div className="flex flex-col min-h-screen w-full">
+        {user ? (
+          <BaseLayout>
+            <p>Select a topic from the menu.</p>
+          </BaseLayout>
+        ) : (
+          <div className="flex flex-col items-center justify-center bg-gray-200 p-4 text-gray-900 grow">
+            {typeof window !== "undefined" && (
+              <ReactPlayer
+                url="https://www.youtube.com/watch?v=8BoovULyJeg&list=PLVEnBuMmQvXukhIgRrTIwOxWBoLwYYL0A"
+                playing={false}
+                controls={true}
+                className="mb-8 max-w-full rounded-lg shadow-2xl"
+              />
+            )}
+            <p className="mx-auto mt-8 rounded-lg bg-white p-4 text-center text-xl font-semibold shadow-lg md:text-2xl lg:w-1/2">
+              Welcome to CyberTutor! Dive into the world of cyber security and
+              fortify your digital life today. Explore our curated video series to
+              become a savvy internet user and protect yourself against online
+              threats.
+            </p>
+            <div className="mt-10 flex space-x-6">
+              <button
+                className="rounded-lg bg-blue-700 px-6 py-3 font-bold text-white shadow-lg transition duration-150 ease-in-out hover:bg-blue-800"
+                onClick={() => router.push("/users/sign-up")}
+              >
+                Register
+              </button>
+              <button
+                className="rounded-lg bg-green-700 px-6 py-3 font-bold text-white shadow-lg transition duration-150 ease-in-out hover:bg-green-800"
+                onClick={() => router.push("/users/sign-in")}
+              >
+                Login
+              </button>
             </div>
-          </footer>
-        </div>
-      )}
+          </div>
+        )}
+        <footer className="w-full border-t border-gray-300 bg-white py-4 text-center text-gray-900">
+          <div className="mx-auto px-4">
+            <p>&copy; 2024 Cyber Tutor. All rights reserved.</p>
+          </div>
+        </footer>
+      </div>
     </>
   );
 }
