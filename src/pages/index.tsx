5c9866bed814e32e448cff56315a6416738ba64f
Andrew <andrew76280@gmail.com>
2024-04-07 08:01:17 -0400

    feat: Displays the total amount of chapters and topics completed by user on home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,9 @@ import { auth, db } from "../components/firebase/config";
 import { useAuthState } from "react-firebase-hooks/auth";
 import React, { useEffect, useState } from "react";
 import dynamic from "next/dynamic";
-import queryUserDocument from "../components/firebase/firebase_functions";
+import queryUserDocument, {
+  numberOfTopicsCompleted,
+} from "../components/firebase/firebase_functions";
 import {
   DocumentData,
   collection,
@@ -14,7 +16,7 @@ import {
 } from "firebase/firestore";
 import { useRouter } from "next/router";
 import CircularWithValueLabel from "~/components/ProgressCircle";
-
+import { set } from "firebase/database";
 
 const ReactPlayer = dynamic(() => import("react-player"), { ssr: false });
 
@@ -29,21 +31,30 @@ export default function Home() {
   const [userDocument, setUserDocument] = useState<UserDocument | null>(null);
   const [proficiencyRatio, setProficiencyRatio] = useState<number>(0);
   const [streakCount, setStreakCount] = useState<number>(0);
+  const [topicsCompleted, setTopicsCompleted] = useState<number>(0);
+  const [totalChapters, setTotalChapters] = useState(0);
+  const [completedChapters, setCompletedChapters] = useState(0);
   const router = useRouter();
 
   const uid = user ? user.uid : null;
 
-  useEffect(() => {
+  if (uid) {
+    const getTopicsCompleted = async () => {
+      const topicsNum = await numberOfTopicsCompleted(uid);
+      setTopicsCompleted(topicsNum);
+    };
+    getTopicsCompleted();
+  }
 
-    const savedRatio = localStorage.getItem('proficiencyRatio');
+  useEffect(() => {
+    const savedRatio = localStorage.getItem("proficiencyRatio");
     if (savedRatio) {
       setProficiencyRatio(parseFloat(savedRatio));
     }
   }, []);
 
   useEffect(() => {
-
-    localStorage.setItem('proficiencyRatio', proficiencyRatio.toString());
+    localStorage.setItem("proficiencyRatio", proficiencyRatio.toString());
   }, [proficiencyRatio]);
 
   useEffect(() => {
@@ -67,6 +78,8 @@ export default function Home() {
             ? (completedDocumentsCount / totalDocuments) * 100
             : 0;
         setProficiencyRatio(ratio);
+        setTotalChapters(totalDocuments);
+        setCompletedChapters(completedDocumentsCount);
       });
     }
   }, [userDocument]);
@@ -75,11 +88,9 @@ export default function Home() {
     if (userDocument) {
       if (!userDocument.data().initialSurveyComplete) {
         router.push("/initialsurvey/begin");
-      }
-      else if (!userDocument.data().demographicSurveyComplete){
+      } else if (!userDocument.data().demographicSurveyComplete) {
         router.push("/demographicsurvey/survey");
-      }
-       else {
+      } else {
         updateUserStreak(uid, userDocument.data().lastLoginDate);
       }
     }
@@ -93,15 +104,16 @@ export default function Home() {
 
     const userDocRef = doc(db, "users", userDocument.id);
     const currentDate = new Date();
-    currentDate.setHours(0, 0, 0, 0); 
+    currentDate.setHours(0, 0, 0, 0);
 
     let newStreakCount = userDocument.streakCount || 0;
     let shouldUpdateFirebase = false;
 
     if (lastLoginDate) {
       const lastLogin = new Date(lastLoginDate.seconds * 1000);
-      lastLogin.setHours(0, 0, 0, 0); 
-      const diffInDays = (currentDate.getTime() - lastLogin.getTime()) / (1000 * 3600 * 24);
+      lastLogin.setHours(0, 0, 0, 0);
+      const diffInDays =
+        (currentDate.getTime() - lastLogin.getTime()) / (1000 * 3600 * 24);
 
       if (diffInDays >= 1 && diffInDays < 2) {
         newStreakCount += 1;
@@ -131,47 +143,54 @@ export default function Home() {
         <title>Cyber Tutor</title>
         <meta name="description" content="Cyber Tutor" />
         <link rel="icon" href="/favicon.ico" />
-        <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@400;500;700&display=swap" rel="stylesheet" />
-        </Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@400;500;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <div className="flex min-h-screen w-full flex-col">
         {user ? (
           <BaseLayout>
-          <div className="bg-white">
-            <div className="max-w-xl mx-auto pt-12 pb-8 px-4">
-              <h1 className="text-2xl font-bold text-gray-800 text-center">
-                <i className="fas fa-shield-alt mr-2 text-indigo-500"></i>
-                Ready to fortify your digital life and stay protected?
-              </h1>
-              <p className="mt-4 text-lg text-gray-600 text-center">
-                <i className="fas fa-list-ul mr-2 text-gray-400"></i>
-                Select a topic from the menu.
-              </p>
-              <div className="mt-6 text-center">
-                <span className="text-xl font-semibold text-gray-800">
-                  <i className="fas fa-chart-line mr-2 text-green-500"></i>
-                  Progress:
-                </span>
-                <br />
-                <br />
-                <CircularWithValueLabel value={proficiencyRatio} size={80} />
-              </div>
-              <div className="mt-4 text-center">
-                {streakCount > 0 ? (
-                  <span className="text-xl font-semibold text-green-600">
-                    <i className="fas fa-fire mr-2"></i>
-                    Streak: {streakCount} day(s)
+            <div className="bg-white">
+              <div className="mx-auto max-w-xl px-4 pb-8 pt-12">
+                <h1 className="text-center text-2xl font-bold text-gray-800">
+                  <i className="fas fa-shield-alt mr-2 text-indigo-500"></i>
+                  Ready to fortify your digital life and stay protected?
+                </h1>
+                <p className="mt-4 text-center text-lg text-gray-600">
+                  <i className="fas fa-list-ul mr-2 text-gray-400"></i>
+                  Select a topic from the menu.
+                </p>
+                <div className="mt-6 text-center">
+                  <span className="text-xl font-semibold text-gray-800">
+                    <i className="fas fa-chart-line mr-2 text-green-500"></i>
+                    Progress:
                   </span>
-                ) : (
-                  <span className="text-lg text-gray-600">
-                    <i className="fas fa-clock mr-2 text-blue-500"></i>
-                    Start your streak by logging in daily.
-                  </span>
-                )}
+                  <br />
+                  Topics Complete: {topicsCompleted} / 5
+                  <br />
+                  Chapters Complete: {completedChapters} / {totalChapters}
+                  <br />
+                  <br />
+                  <CircularWithValueLabel value={proficiencyRatio} size={80} />
+                </div>
+
+                <div className="mt-4 text-center">
+                  {streakCount > 0 ? (
+                    <span className="text-xl font-semibold text-green-600">
+                      <i className="fas fa-fire mr-2"></i>
+                      Streak: {streakCount} day(s)
+                    </span>
+                  ) : (
+                    <span className="text-lg text-gray-600">
+                      <i className="fas fa-clock mr-2 text-blue-500"></i>
+                      Start your streak by logging in daily.
+                    </span>
+                  )}
+                </div>
               </div>
             </div>
-          </div>
-        </BaseLayout>
-               
+          </BaseLayout>
         ) : (
           <div className="flex grow flex-col items-center justify-center bg-gray-200 p-4 text-gray-900">
             {typeof window !== "undefined" && (
