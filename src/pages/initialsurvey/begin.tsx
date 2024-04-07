fa99996d8809b099b484101e8ad3384fb174a453
Andrew <andrew76280@gmail.com>
2024-04-07 09:09:19 -0400

    refactor: Survey begin page text changed

--- src/pages/initialsurvey/begin.tsx
+++ src/pages/initialsurvey/begin.tsx
@@ -13,9 +13,9 @@ export default function InitialSurvey() {
     return (
         <div className="bg-gray-800 flex justify-center items-center h-screen">
             <div className="text-center animate-fadeIn">
-                <h1 className="text-white text-3xl mb-4">Let's get to know how much cyber security you know</h1>
+                <h1 className="text-white text-3xl mb-4">Let's start by assessing how much cyber security you know</h1>
                 <button onClick={handleRouter} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
-                    Start
+                    Start Survey
                 </button>
             </div>
         </div>
