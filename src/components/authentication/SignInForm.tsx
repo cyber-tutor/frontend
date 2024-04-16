36745eaee6689385cab0b65bde6387bdb2e1430b
Matthew Quijano <mq022002@gmail.com>
2024-04-16 02:54:08 -0400

    style: Fix janky UI for SignInForm
    - Removed BaseLayout.
    - Removed redundant Tailwind classes.

--- src/components/authentication/SignInForm.tsx
+++ src/components/authentication/SignInForm.tsx
@@ -65,96 +65,74 @@ const SignInForm = () => {
   }, [error]);
 
   return (
-    <div
-      className="flex h-screen flex-col lg:pl-64"
-      style={{ marginTop: "-4rem" }}
-    >
-      <div className="flex flex-grow items-center justify-center">
-        <div className="w-full max-w-sm rounded bg-white p-6 shadow-md">
-          <h2 className="mb-6 text-center text-3xl font-bold">Sign In</h2>
-          {signInError && (
-            <p className="mb-4 text-center text-sm text-red-500">
-              {signInError}
-            </p>
-          )}
-          <form onSubmit={handleSignIn} className="space-y-6">
-            <div>
-              <label
-                htmlFor="email"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Email
-              </label>
-              <input
-                type="email"
-                id="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                required
-                className="mt-1 w-full rounded-md border px-3 py-2"
-              />
-            </div>
-            <div>
-              <label
-                htmlFor="password"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Password
-              </label>
-              <Password
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                toggleMask
-                feedback={false} // Add this line
-                className="w-full rounded border px-3 py-2 leading-tight focus:border-blue-500 focus:outline-none"
-              />
-            </div>
-            <button
-              type="submit"
-              className="w-full rounded bg-green-500 px-4 py-2 text-white hover:bg-green-600 focus:outline-none"
-            >
-              Sign In
-            </button>
-            <div className="my-4 flex items-center justify-between">
-              <div className="flex-grow border-t border-gray-300"></div>
-              <span className="mx-4 flex-shrink uppercase text-gray-600">
-                or
-              </span>
-              <div className="flex-grow border-t border-gray-300"></div>
-            </div>
-            <button
-              onClick={handleGoogleSignIn}
-              className="flex w-full items-center justify-center rounded border bg-white px-4 py-2 text-sm text-gray-700 hover:bg-gray-50"
-            >
-              <FcGoogle className="text-xl" />
-              <span className="ml-2">Login with Google</span>
-            </button>
-          </form>
-
-          <div className="my-4 border-t border-gray-300"></div>
-          <p className="mt-4 text-center text-sm text-gray-600">
-            Don't have an account?{" "}
-            <button
-              className="flex w-full items-center justify-center rounded border bg-blue-500 px-4 py-2  text-white hover:bg-blue-600 focus:outline-none"
-              onClick={() => router.push("/users/sign-up")}
-            >
-              Sign Up
-            </button>
-          </p>
-
+    <div className="flex h-screen items-center justify-center">
+      <div className="rounded-lg p-8 shadow-lg">
+        <h1 className="mb-8 text-center text-2xl font-bold">Sign In</h1>
+        <form onSubmit={handleSignIn} className="space-y-6">
+          <div className="mb-4">
+            <label htmlFor="email" className="mb-1 font-bold">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+              className="w-full rounded border border-gray-300 px-3 py-2"
+            />
+            {signInError && <p className="text-red-600">{signInError}</p>}
+          </div>
+          <div className="mb-6">
+            <label htmlFor="password" className="mb-1 font-bold">
+              Password
+            </label>
+            <Password
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              toggleMask
+              feedback={false}
+              className="w-full rounded border border-gray-300 px-3 py-2"
+            />
+          </div>
           <button
-            className="mt-4 flex w-full items-center justify-center rounded border border-t bg-blue-500 px-4 py-2 text-white hover:bg-blue-600 focus:outline-none"
-            onClick={() => router.push("/")}
+            type="submit"
+            className="w-full rounded bg-green-500 py-2 font-bold text-white hover:bg-green-600"
           >
-            Home
+            Sign In
           </button>
-        </div>
+          <div className="my-6 flex items-center justify-between">
+            <span className="w-1/5 border-b border-gray-300"></span>
+            <p className="mx-4 text-xs text-gray-700">OR</p>
+            <span className="w-1/5 border-b border-gray-300"></span>
+          </div>
+          <button
+            onClick={handleGoogleSignIn}
+            className="flex w-full items-center justify-center rounded border border-gray-300 py-2 text-gray-700 shadow-sm hover:bg-gray-50"
+          >
+            <FcGoogle className="text-2xl" />
+            <span className="ml-2">Login with Google</span>
+          </button>
+        </form>
+
+        <div className="my-4 border-t border-gray-300"></div>
+        <p className="my-4 text-center text-sm text-gray-600">
+          Don't have an account?{" "}
+        </p>
+        <button
+          className="flex w-full items-center justify-center rounded border bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-600"
+          onClick={() => router.push("/users/sign-up")}
+        >
+          Sign Up
+        </button>
+
+        <button
+          className="mt-4 flex w-full items-center justify-center rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-600"
+          onClick={() => router.push("/")}
+        >
+          Home
+        </button>
       </div>
-      <footer className="w-full bg-slate-50 py-4 text-center text-gray-900">
-        <div className="mx-auto">
-          <p>&copy; 2024 Cyber Tutor. All rights reserved.</p>
-        </div>
-      </footer>
     </div>
   );
 };
--- src/pages/users/sign-in/index.tsx
+++ src/pages/users/sign-in/index.tsx
@@ -1,14 +1,10 @@
-import React from "react";
-import { BaseLayout } from "../../../components/layouts/BaseLayout";
 import SignInForm from "../../../components/authentication/SignInForm";
 
 const SignInPage = () => {
   return (
-    <BaseLayout showSidebar={false}>
-      <div className="flex w-full justify-center">
-        <SignInForm />
-      </div>
-    </BaseLayout>
+    <div className="flex w-full justify-center">
+      <SignInForm />
+    </div>
   );
 };
 
