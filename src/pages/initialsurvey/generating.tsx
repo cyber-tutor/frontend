f859d14266c5eca11b3c4f39011254d8719a92f0
Matthew Quijano <mq022002@gmail.com>
2024-04-16 00:32:19 -0400

    refactor: Remove redundant pages route

--- src/pages/initialsurvey/generating.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-interface Props {}
-
-const MyComponent: React.FC<Props> = ({}) => {
-  return <div>Ai is generating your content... Loading...</div>;
-};
-
-export default MyComponent;
