be484015ca19d7a063a0acdd4fb2a34f908dd477
Matthew Quijano <mq022002@gmail.com>
2024-04-17 15:39:00 -0400

    style: Only add padding if the screensize is md or sm

--- src/components/layouts/BaseLayout.tsx
+++ src/components/layouts/BaseLayout.tsx
@@ -93,7 +93,9 @@ export const BaseLayout = ({ children, showSidebar = true }: LayoutProps) => {
         showSidebar={showSidebar}
         userDocument={userDocument}
       />
-      <div className="flex h-screen w-full flex-col items-center gap-4 overflow-y-auto pt-10">
+      <div
+        className={`flex h-screen w-full flex-col items-center gap-4 overflow-y-auto ${screenSize === "md" || screenSize === "sm" ? "pt-10" : ""}`}
+      >
         {children}
       </div>
     </div>
