2c6e1a3be50b0ec7c8b77b4674f5eed4408946f0
Matthew Quijano <mq022002@gmail.com>
2024-04-20 22:02:34 -0400

    fix: let, to const and Prettier auto-formatting

--- src/pages/news/content/index.tsx
+++ src/pages/news/content/index.tsx
@@ -19,20 +19,19 @@ const CyberSecurityNews = () => {
 
   const fetchNews = useCallback(async () => {
     if (!user || loading) return;
-  
+
     try {
       // Fetch news from News API
       const response = await axios.get(
         `https://newsapi.org/v2/everything?q=cybersecurity&apiKey=${process.env.NEXT_PUBLIC_NEWS_KEY}`,
       );
-      let articles = response.data.articles;
-  
+      const articles = response.data.articles;
 
       for (let i = articles.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [articles[i], articles[j]] = [articles[j], articles[i]];
       }
-  
+
       if (articles.length > 0) {
         setArticles(articles.slice(0, 10)); // Get the first 10 articles
       }
@@ -62,7 +61,7 @@ const CyberSecurityNews = () => {
   }
 
   return (
-    <div className="flex min-h-screen flex-col bg-gray-800 text-white mb-7">
+    <div className="mb-7 flex min-h-screen flex-col bg-gray-800 text-white">
       <div className="w-full p-4">
         <button
           onClick={() => router.push("/")}
@@ -74,11 +73,17 @@ const CyberSecurityNews = () => {
       <div className="mx-auto my-auto max-w-6xl overflow-hidden rounded-xl bg-white p-6 text-black shadow-md">
         <h1 className="mb-6 text-3xl font-semibold">Cybersecurity News</h1>
         {articles.map((article, index) => (
-          <NewsCard key={index} urlToImage={(article as any).urlToImage} title={(article as any).title} url={(article as any).url} description={(article as any).description} />
+          <NewsCard
+            key={index}
+            urlToImage={(article as any).urlToImage}
+            title={(article as any).title}
+            url={(article as any).url}
+            description={(article as any).description}
+          />
         ))}
         <button
           onClick={() => router.reload()}
-          className="mt-4 rounded bg-blue-800 px-4 py-2 text-lg font-bold text-white hover:bg-blue-700 transition duration-300"
+          className="mt-4 rounded bg-blue-800 px-4 py-2 text-lg font-bold text-white transition duration-300 hover:bg-blue-700"
         >
           Refresh List of Articles
         </button>
