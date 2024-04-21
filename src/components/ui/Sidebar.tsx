5a9710056f22746ecf9f1e2bc829cda0a46ab669
Matthew Quijano <mq022002@gmail.com>
2024-04-21 07:47:04 -0400

    refactor: Move sidebar into separate component

---
+++ src/components/ui/Sidebar.tsx
@@ -0,0 +1,125 @@
+import { useState } from "react";
+import { Sidebar, Menu, MenuItem, SubMenu } from "react-pro-sidebar";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import { IoPersonCircleSharp, IoLogOutOutline } from "react-icons/io5";
+
+type Topic = {
+  topicId: string;
+  topicTitle: string;
+  isComplete: boolean;
+};
+
+type SidebarProps = {
+  topics: Topic[];
+  screenSize: string;
+  showSidebar: boolean;
+  userDocument: any;
+  user: any;
+  handleLogoClick: () => void;
+  handleTopicClick: (topic: Topic) => void;
+  handleLogout: () => void;
+};
+
+export const CustomSidebar = ({
+  topics,
+  screenSize,
+  showSidebar,
+  userDocument,
+  user,
+  handleLogoClick,
+  handleTopicClick,
+  handleLogout,
+}: SidebarProps) => {
+  const [isSubMenuOpen, setIsSubMenuOpen] = useState(true);
+  const router = useRouter();
+
+  return (
+    <div className="flex min-h-screen items-stretch bg-slate-50">
+      <div className="flex h-screen flex-col items-center bg-slate-400">
+        <div className="flex h-full flex-col justify-between">
+          {(screenSize === "lg" || screenSize === "md") && showSidebar ? (
+            <Sidebar className="flex h-full flex-col">
+              <Menu>
+                <MenuItem className="flex flex-col justify-center p-2 text-center">
+                  <div className="flex items-center justify-center">
+                    <Image
+                      src="/Cyber-Tutor_Logo.png"
+                      alt="Cyber Tutor Logo"
+                      width={40}
+                      height={40}
+                      layout="fixed"
+                      onClick={handleLogoClick}
+                    />
+                    <span onClick={handleLogoClick}>Cyber Tutor </span>
+                  </div>
+                </MenuItem>
+                <MenuItem onClick={() => router.push("/")}>Home</MenuItem>
+                <SubMenu
+                  label="Topics"
+                  open={isSubMenuOpen}
+                  onOpenChange={() => setIsSubMenuOpen(!isSubMenuOpen)}
+                >
+                  {user &&
+                  userDocument &&
+                  userDocument.data().initialSurveyComplete ? (
+                    topics.map((topic) => (
+                      <MenuItem
+                        key={topic.topicId}
+                        onClick={() => handleTopicClick(topic)}
+                      >
+                        <span className="text-sm">{topic.topicTitle}</span>
+                        {topic.isComplete && <span> 💯</span>}
+                      </MenuItem>
+                    ))
+                  ) : (
+                    <div className="relative">
+                      <div className="pointer-events-none select-none blur-sm">
+                        {topics.map((topic) => (
+                          <MenuItem key={topic.topicId}>
+                            {topic.topicTitle}
+                          </MenuItem>
+                        ))}
+                      </div>
+                      <div className="absolute bottom-0 left-0 right-0 top-0 flex items-center justify-center bg-white bg-opacity-75">
+                        <span>Sign in to unlock</span>
+                      </div>
+                    </div>
+                  )}
+                </SubMenu>
+
+                <div className="absolute bottom-0 w-full">
+                  {user && (
+                    <>
+                      <br />
+                      <div className="border-t-2 border-gray-400"></div>
+                      <div className="flex min-h-[3rem] justify-between">
+                        <button
+                          className="flex  flex-grow items-center bg-slate-50 px-2 py-3 text-left text-gray-700 transition duration-200 ease-in-out hover:bg-gray-300"
+                          onClick={() =>
+                            router.push(`/users/profile/${userDocument.id}`)
+                          }
+                        >
+                          <IoPersonCircleSharp />
+                          <span className="ml-2">
+                            {userDocument ? userDocument.data().name : ""}
+                          </span>
+                        </button>
+                        <button
+                          className="flex w-12 items-center justify-center border-l-2 bg-slate-50 py-3 text-gray-700 transition duration-200 ease-in-out hover:bg-gray-300"
+                          onClick={handleLogout}
+                        >
+                          <IoLogOutOutline className="text-red-500" />
+                        </button>
+                      </div>
+                    </>
+                  )}
+                </div>
+              </Menu>
+            </Sidebar>
+          ) : null}
+        </div>
+      </div>
+    </div>
+  );
+};
