114dc84f931221def4d5f31d08e5fa8db5ca66db
Matthew Quijano <mq022002@gmail.com>
2024-04-21 12:12:14 -0400

    style: Adjust box model for topics list

--- src/pages/topics/[topic].tsx
+++ src/pages/topics/[topic].tsx
@@ -227,7 +227,7 @@ export default function TopicPage() {
   return (
     <BaseLayout>
       <div className="flex w-full flex-col items-center">
-        <div className="mt-0 overflow-y-auto rounded-lg border-2 border-solid text-center md:mt-16 lg:mt-16 lg:w-7/12 lg:p-10">
+        <div className="mt-20 overflow-y-auto rounded-lg border-2 border-solid  text-center md:mt-20 lg:mt-10 p-5">
           <div className="grid grid-cols-6 items-center">
             <h1 className="col-span-5 flex justify-start text-3xl font-bold">
               {topic.topicTitle}
