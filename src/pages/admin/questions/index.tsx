a2377f304ba33d6318db793023fd23f837281cd6
Matthew Quijano <mq022002@gmail.com>
2024-04-28 07:38:12 -0400

    style: Adjust box model for questions CRUD

--- src/pages/admin/questions/index.tsx
+++ src/pages/admin/questions/index.tsx
@@ -190,7 +190,9 @@ export default function CRUD_Questions() {
 
   return (
     <BaseLayout>
-      <h1 className="pt-5 text-center font-bold">Admin Interface: Question CRUD</h1>
+      <h1 className="text-center font-bold md:mt-20 lg:mt-10">
+        Admin Interface: Question CRUD
+      </h1>
       <div className="h-100 h-full w-full overflow-auto sm:overflow-scroll">
         <div className="sticky top-0 z-10 bg-white">
           {editingId && (
