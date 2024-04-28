4067d318647e8d045c35edce63ff36e5d4804b11
Matthew Quijano <mq022002@gmail.com>
2024-04-28 08:32:59 -0400

    style: Center the submit button

--- src/components/admin/CRUDTopicsForm.tsx
+++ src/components/admin/CRUDTopicsForm.tsx
@@ -124,12 +124,14 @@ const CRUDTopicsForm: React.FC = () => {
             onChange={handleTopicChange}
             className="mb-2 block w-full rounded border border-gray-300 p-2"
           />
-          <button
-            type="submit"
-            className="rounded bg-blue-500 px-4 py-2 text-white"
-          >
-            {currentTopicId ? "Save Changes" : "Add Topic"}
-          </button>
+          <div className="text-center">
+            <button
+              type="submit"
+              className="rounded bg-blue-500 px-4 py-2 text-white"
+            >
+              {currentTopicId ? "Save Changes" : "Add Topic"}
+            </button>
+          </div>
         </form>
       </div>
       <div>
