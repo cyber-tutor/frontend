d92b6f47fdd52f49e008b7180ab9bec7402ad4cf
Matthew Quijano <mq022002@gmail.com>
2024-04-28 06:30:52 -0400

    feat: Topics CRUD form for superusers

---
+++ src/components/admin/CRUDTopicsForm.tsx
@@ -0,0 +1,105 @@
+import React, { useState, useEffect, ChangeEvent, FormEvent } from "react";
+import { db } from "../firebase/config";
+import {
+  collection,
+  doc,
+  getDocs,
+  addDoc,
+  updateDoc,
+  deleteDoc,
+  query,
+  orderBy,
+  DocumentData,
+  setDoc,
+} from "firebase/firestore";
+
+interface Topic {
+  topicId?: string;
+  topicTitle: string;
+  topicDescription: string;
+  order: number;
+}
+
+const CRUDTopicsForm: React.FC = () => {
+  const [topics, setTopics] = useState<Topic[]>([]);
+  const [currentTopicId, setCurrentTopicId] = useState<string>("");
+
+  const [newTopic, setNewTopic] = useState<Topic>({
+    topicTitle: "",
+    topicDescription: "",
+    order: 0,
+  });
+
+  useEffect(() => {
+    const fetchTopics = async () => {
+      const topicsCollection = collection(db, "topics");
+      const topicsQuery = query(topicsCollection, orderBy("order"));
+      const snapshot = await getDocs(topicsQuery);
+      const topicsData = snapshot.docs.map((doc) => ({
+        topicId: doc.id,
+        ...doc.data(),
+      })) as Topic[];
+      setTopics(topicsData);
+    };
+
+    fetchTopics();
+  }, []);
+
+  const handleTopicChange = (
+    event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) => {
+    const { name, value } = event.target;
+    setNewTopic({ ...newTopic, [name]: value });
+  };
+
+  const handleAddTopic = async (event: FormEvent) => {
+    event.preventDefault();
+    if (!newTopic.topicTitle || !newTopic.topicDescription) return;
+
+    const topicId = newTopic.topicTitle.toLowerCase().replace(/ /g, "_");
+
+    const topicsCollection = collection(db, "topics");
+    const topicDoc = doc(topicsCollection, topicId);
+    await setDoc(topicDoc, newTopic);
+    setNewTopic({ topicTitle: "", topicDescription: "", order: 0 });
+  };
+
+  return (
+    <div className="flex w-full flex-col items-center">
+      <h2 className="mb-4">Add or Update Topics</h2>
+      <form onSubmit={handleAddTopic} className="mb-12 w-4/5">
+        <input
+          type="text"
+          name="topicTitle"
+          placeholder="Topic Title"
+          value={newTopic.topicTitle}
+          onChange={handleTopicChange}
+          className="mb-2 block w-full rounded border border-gray-300 p-2"
+        />
+        <textarea
+          name="topicDescription"
+          placeholder="Topic Description"
+          value={newTopic.topicDescription}
+          onChange={handleTopicChange}
+          className="mb-2 block w-full rounded border border-gray-300 p-2"
+        />
+        <input
+          type="number"
+          name="order"
+          placeholder="Order"
+          value={newTopic.order}
+          onChange={handleTopicChange}
+          className="mb-2 block w-full rounded border border-gray-300 p-2"
+        />
+        <button
+          type="submit"
+          className="rounded bg-blue-500 px-4 py-2 text-white"
+        >
+          Add Topic
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default CRUDTopicsForm;
---
+++ src/pages/admin/topics/index.tsx
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from "react";
+import { auth, db } from "../../../components/firebase/config";
+import {
+  getDocs,
+  collection,
+  onSnapshot,
+  doc,
+  getDoc,
+  query,
+  where,
+} from "firebase/firestore";
+import Head from "next/head";
+import { BaseLayout } from "../../../components/layouts/BaseLayout";
+import CRUDTopicsForm from "../../../components/admin/CRUDTopicsForm";
+import { User } from "firebase/auth";
+import { useRouter } from "next/router";
+
+interface UserData {
+  userId: string;
+  isSuperUser: boolean;
+}
+
+export default function CRUDTopics() {
+  const [user, setUser] = useState<User | null>(null);
+  const router = useRouter();
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(async (currentUser) => {
+      setUser(currentUser);
+      if (currentUser) {
+        const usersCollection = collection(db, "users");
+        const superUserQuery = query(
+          usersCollection,
+          where("userId", "==", currentUser.uid),
+          where("isSuperuser", "==", true),
+        );
+        const querySnapshot = await getDocs(superUserQuery);
+        if (!querySnapshot.empty) {
+        } else {
+          console.log("You are not a superuser.");
+          router.push("/");
+        }
+      } else {
+        router.push("/");
+      }
+    });
+
+    return () => unsubscribe();
+  }, [router]);
+
+  return (
+    <BaseLayout>
+      <Head>
+        <title>Admin Interface: Topics CRUD</title>
+      </Head>
+      <h1 className="pt-5 text-center font-bold">
+        Admin Interface: Topics CRUD
+      </h1>
+      <div className="h-100 w-full">
+        <CRUDTopicsForm />
+      </div>
+    </BaseLayout>
+  );
+}
