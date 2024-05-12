74ab63b96de215fcc4b0222b28763a4d1c30c5bd
Matthew Quijano <mq022002@gmail.com>
2024-05-12 05:38:57 -0400

    refactor: Utilize useIsSuperuser hook in chapters CRUD

--- src/pages/admin/chapters/index.tsx
+++ src/pages/admin/chapters/index.tsx
@@ -1,52 +1,11 @@
-import React, { useState, useEffect } from "react";
-import { auth, db } from "../../../components/firebase/config";
-import {
-  getDocs,
-  collection,
-  onSnapshot,
-  doc,
-  getDoc,
-  query,
-  where,
-} from "firebase/firestore";
+import React from "react";
 import Head from "next/head";
 import { BaseLayout } from "../../../components/layouts/BaseLayout";
 import CRUDChaptersForm from "../../../components/admin/CRUDChaptersForm";
-import { User } from "firebase/auth";
-import { useRouter } from "next/router";
-
-interface UserData {
-  userId: string;
-  isSuperUser: boolean;
-}
+import { useIsSuperuser } from "../../../hooks/useIsSuperuser";
 
 export default function CRUDTopics() {
-  const [user, setUser] = useState<User | null>(null);
-  const router = useRouter();
-
-  useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged(async (currentUser) => {
-      setUser(currentUser);
-      if (currentUser) {
-        const usersCollection = collection(db, "users");
-        const superUserQuery = query(
-          usersCollection,
-          where("userId", "==", currentUser.uid),
-          where("isSuperuser", "==", true),
-        );
-        const querySnapshot = await getDocs(superUserQuery);
-        if (!querySnapshot.empty) {
-        } else {
-          console.log("You are not a superuser.");
-          router.push("/");
-        }
-      } else {
-        router.push("/");
-      }
-    });
-
-    return () => unsubscribe();
-  }, [router]);
+  const isSuperuser = useIsSuperuser();
 
   return (
     <BaseLayout>
@@ -56,9 +15,11 @@ export default function CRUDTopics() {
       <h1 className="mt-20 text-center font-bold md:mt-20 lg:mt-10">
         Admin Interface: Chapters CRUD
       </h1>
-      <div className="h-100 w-full">
-        <CRUDChaptersForm />
-      </div>
+      {isSuperuser && (
+        <div className="h-100 w-full">
+          <CRUDChaptersForm />
+        </div>
+      )}
     </BaseLayout>
   );
 }
