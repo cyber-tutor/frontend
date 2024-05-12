7410899ce7cc38e4f40794e7ae82d2bdb4c91d92
Matthew Quijano <mq022002@gmail.com>
2024-05-12 02:02:38 -0400

    refactor: Update topics and chapters fetching in CRUDQuestionForm

--- src/components/admin/CRUDQuestionForm.tsx
+++ src/components/admin/CRUDQuestionForm.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect, FormEvent, ChangeEvent } from "react";
 import InputField from "../ui/InputField";
 import { collection, getDocs, query, orderBy } from "firebase/firestore";
 import { db } from "../firebase/config";
-import { useTopics } from "../hooks/useTopics";
+import { useTopicsWithChapters } from "../hooks/useTopicsWithChapters";
 import { Topic } from "../../types";
 import { Chapter } from "../../types";
 
@@ -24,60 +24,9 @@ const QuestionForm: React.FC<{
   question?: Question;
   onSubmit: (updatedQuestion: Question) => void;
 }> = ({ question, onSubmit }) => {
-  const topics = useTopics();
+  const topics = useTopicsWithChapters();
   const [localTopics, setLocalTopics] = useState<Topic[]>([]);
 
-  useEffect(() => {
-    const fetchTopics = async () => {
-      const topicsCollectionRef = collection(db, "topics");
-      const topicsArray: Topic[] = [];
-
-      const topicsSnapshot = await getDocs(
-        query(topicsCollectionRef, orderBy("order")),
-      );
-      for (const topicDoc of topicsSnapshot.docs) {
-        const topicData = topicDoc.data();
-        const topicId = topicDoc.id;
-
-        const chaptersCollectionRef = collection(topicDoc.ref, "chapters");
-        const chaptersSnapshot = await getDocs(chaptersCollectionRef);
-        const chapters: Chapter[] = [];
-        chaptersSnapshot.forEach((chapterDoc) => {
-          const chapterData = chapterDoc.data();
-          const chapterId = chapterDoc.id;
-          chapters.push({
-            chapterId: chapterId,
-            chapterType: chapterData.chapterType,
-            chapterTitle: chapterData.chapterTitle,
-            chapterDescription: chapterData.chapterDescription,
-            controlGroupContent: chapterData.controlGroupContent,
-            experimentalGroupContent: chapterData.experimentalGroupContent,
-            controlGroupImageURLs: chapterData.controlGroupImageURL,
-            experimentalGroupImageURLs: chapterData.experimentalGroupImageURL,
-            controlGroupVideoURLs: chapterData.controlGroupVideoURLs,
-            experimentalGroupVideoURLs: chapterData.experimentalGroupVideoURLs,
-            order: chapterData.order,
-            proficiency: chapterData.proficiency,
-          });
-        });
-
-        const newTopic = {
-          topicId: topicId,
-          topicTitle: topicData.topicTitle,
-          topicDescription: topicData.topicDescription,
-          order: topicData.order,
-          isComplete: topicData.isComplete,
-          chapters: chapters,
-        };
-
-        topicsArray.push(newTopic);
-      }
-
-      setLocalTopics(topicsArray);
-    };
-
-    fetchTopics();
-  }, []);
   const [editedQuestion, setEditedQuestion] = useState<Question>(
     question || {
       question: "",
