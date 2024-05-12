6f1f733b7cbff645bd281d8ae88fb6bcb449b710
Matthew Quijano <mq022002@gmail.com>
2024-05-12 01:36:26 -0400

    feat: Create reusable hook to fetch list of topics

---
+++ src/components/hooks/useTopics.tsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import { db } from "../firebase/config";
+import { collection, getDocs, query, orderBy } from "firebase/firestore";
+import { Topic } from "../../types";
+
+export const useTopics = () => {
+  const [topics, setTopics] = useState<Topic[]>([]);
+
+  useEffect(() => {
+    const fetchTopics = async () => {
+      const topicsCollectionRef = collection(db, "topics");
+      const topicsQuery = query(topicsCollectionRef, orderBy("order"));
+      const topicsSnapshot = await getDocs(topicsQuery);
+      const fetchedTopics = topicsSnapshot.docs.map((doc) => {
+        const data = doc.data() as Topic;
+        return {
+          ...data,
+          topicId: doc.id,
+          isComplete: false,
+        };
+      });
+
+      setTopics(fetchedTopics);
+    };
+
+    fetchTopics();
+  }, []);
+
+  return topics;
+};
