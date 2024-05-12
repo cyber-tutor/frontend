d2f84b780c3c38be2361b8028ad689985b42c701
Matthew Quijano <mq022002@gmail.com>
2024-05-12 05:40:35 -0400

    refactor: Utilize useIsSuperuser hook in control group content CRUD

--- src/pages/admin/content/index.tsx
+++ src/pages/admin/content/index.tsx
@@ -1,56 +1,14 @@
 import React, { useState, useEffect } from "react";
-import { auth, db } from "../../../components/firebase/config";
-import {
-  getDocs,
-  collection,
-  onSnapshot,
-  doc,
-  getDoc,
-  query,
-  where,
-} from "firebase/firestore";
+import { db } from "../../../components/firebase/config";
+import { getDocs, collection } from "firebase/firestore";
 import Head from "next/head";
 import { BaseLayout } from "../../../components/layouts/BaseLayout";
 import ControlGroupForm from "../../../components/admin/CRUDControlGroupForm";
-import queryUserDocument from "~/components/firebase/FirebaseFunctions";
-import { User } from "firebase/auth";
-import { useRouter } from "next/router";
-
-interface UserData {
-  userId: string;
-  isSuperUser: boolean;
-}
+import { useIsSuperuser } from "../../../hooks/useIsSuperuser";
 
 export default function CRUD_ControlGroupContent() {
   const [topicId, setTopicId] = useState<string>("");
-  const [user, setUser] = useState<User | null>(null);
-
-  const router = useRouter();
-
-  useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged(async (currentUser) => {
-      setUser(currentUser);
-      if (currentUser) {
-        const usersCollection = collection(db, "users");
-        const superUserQuery = query(
-          usersCollection,
-          where("userId", "==", currentUser.uid),
-          where("isSuperuser", "==", true),
-        );
-        const querySnapshot = await getDocs(superUserQuery);
-        if (!querySnapshot.empty) {
-          // console.log("You are a superuser");
-        } else {
-          // console.log("You are not a superuser");
-          router.push("/");
-        }
-      } else {
-        router.push("/");
-      }
-    });
-
-    return () => unsubscribe();
-  }, [router]);
+  const isSuperuser = useIsSuperuser();
 
   useEffect(() => {
     const fetchTopicId = async () => {
@@ -78,11 +36,13 @@ export default function CRUD_ControlGroupContent() {
       <h1 className="mt-20 text-center font-bold md:mt-20 lg:mt-10">
         Admin Interface: Control Group Content CRUD
       </h1>
-      <div className="h-100 h-full w-full">
-        <div className="sticky top-0 z-10">
-          <ControlGroupForm topicId={topicId} />
+      {isSuperuser && (
+        <div className="h-100 h-full w-full">
+          <div className="sticky top-0 z-10">
+            <ControlGroupForm topicId={topicId} />
+          </div>
         </div>
-      </div>
+      )}
     </BaseLayout>
   );
 }
