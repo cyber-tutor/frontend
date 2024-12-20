8e12d2d0c06dcad216d94e83e2cb2cc9c2c2135f
colmak <69098858+colmak@users.noreply.github.com>
2024-12-20 14:54:41 -0500

    refactor: remove commented code in TRPC API handler for improved clarity

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -4,16 +4,12 @@ import { env } from "~/env";
 import { appRouter } from "~/server/api/root";
 import { createTRPCContext } from "~/server/api/trpc";
 
-// export API handler
 export default createNextApiHandler({
   router: appRouter,
   createContext: createTRPCContext,
   onError:
     env.NODE_ENV === "development"
       ? ({ path, error }) => {
-          // console.error(
-          //   `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`
-          // );
         }
       : undefined,
 });
