21c21c8fa76456e145b4e2c760b862e1f7dae317
Matthew Quijano <mq022002@gmail.com>
2024-12-26 07:18:10 -0500

    refactor: Remove all redundant `tRPC` integrations

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,14 @@
-import { type AppType } from "next/app";
+import type { AppProps } from "next/app";
 import { NextUIProvider } from "@nextui-org/react";
-
-import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 import "survey-core/defaultV2.min.css";
 import "primereact/resources/themes/lara-light-cyan/theme.css";
 
-const MyApp: AppType = ({ Component, pageProps }) => {
+export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <NextUIProvider>
       <Component {...pageProps} />
     </NextUIProvider>
   );
-};
-
-export default api.withTRPC(MyApp);
+}
