02d3816900cd3c704aabeadf794713be05079a90
Matthew Quijano <mq022002@gmail.com>
2024-12-26 07:17:48 -0500

    refactor: Only use `Axios` instead of Next.js' API routes for news API

---
+++ src/utils/fetchNews.ts
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const fetchNews = async () => {
+  try {
+    const response = await axios.get(
+      `https://newsapi.org/v2/everything?q=cybersecurity&apiKey=${process.env.NEXT_PUBLIC_NEWS_KEY}`,
+    );
+    return response.data;
+  } catch (error) {
+    throw new Error("Error fetching news");
+  }
+};
+
+export default fetchNews;
